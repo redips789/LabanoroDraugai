09a340873c4bddab3a0fa172dd444c72a3015903
redips789 <povilas.jegelevicius@gmail.com>
2016-04-27 17:17:33 +0300

    fix-veikia nuotraukos įkėlimas

--- LabanoroDraugai/src/main/java/BusinessLogic/CDI/MyFileUploadRenderer.java
+++ LabanoroDraugai/src/main/java/BusinessLogic/CDI/MyFileUploadRenderer.java
@@ -10,6 +10,7 @@ public class MyFileUploadRenderer extends FileUploadRenderer {
 
     @Override
     public void decode(FacesContext context, UIComponent component) {
+        //String a = context.getExternalContext().getRequestContentType().toLowerCase();
         if (context.getExternalContext().getRequestContentType().toLowerCase().startsWith("multipart/")) {
             super.decode(context, component);
         }
