f0df4a7e6f6ba08aebc820fe093f82d59d4929f1
Kristina Naudziunaite <knaudzi@gmail.com>
2016-05-26 11:23:39 +0300

    smulkūs pataisymai

--- LabanoroDraugai/src/main/java/BusinessLogic/EJB/ReservationBean.java
+++ LabanoroDraugai/src/main/java/BusinessLogic/EJB/ReservationBean.java
@@ -5,9 +5,11 @@ import Alternatives.GroupDistribution;
 import DataAccess.EJB.AccountCRUD;
 import DataAccess.EJB.ReservationCRUD;
 import DataAccess.EJB.SettingsCRUD;
+import DataAccess.EJB.SummerhouseCRUD;
 import DataAccess.JPA.Account;
 import DataAccess.JPA.Reservation;
 import DataAccess.JPA.Settings;
+import DataAccess.JPA.Summerhouse;
 import Messages.Message;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -21,7 +23,7 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -36,7 +38,7 @@ import org.primefaces.event.SelectEvent;
  * @author Laurute
  */
 @Named 
-@RequestScoped
+@ViewScoped
 @Stateful
 public class ReservationBean implements Serializable {
     
@@ -62,6 +64,9 @@ public class ReservationBean implements Serializable {
     SummerhouseDetails summerhouseDetails;
     
     @Inject
+    SummerhouseCRUD summerhouseEjb;
+    
+    @Inject
     GroupDistribution groupDistribution;
     
     private Account account;
@@ -73,6 +78,8 @@ public class ReservationBean implements Serializable {
     private Date endDate;
     
     private boolean canReserve;
+    
+    private Summerhouse summerhouse;
     
     private List<Reservation> membersReservations = new ArrayList<>();
 
@@ -140,12 +147,28 @@ public class ReservationBean implements Serializable {
     public void setCanReserve(boolean canReserve) {
         this.canReserve = canReserve;
     }
+
+    public Summerhouse getSummerhouse() {
+        return summerhouse;
+    }
+
+    public void setSummerhouse(Summerhouse summerhouse) {
+        this.summerhouse = summerhouse;
+    }
+    
+    
         //-business-//
     
     @PostConstruct
     public void init() {
         account = accountEjb.findAccount(loginBean.getFbid());
         settings = settingsEjb.findSettings();
+         // cia reikia rasti pagal id is URL arba is summerhouseDetails egzemplioriaus - summerhouseDetails.getDetailedSummerhouse()
+//        summerhouse = summerhouseDetails.getDetailedSummerhouse(); // pirma karta kuriant beansa yra gera info, o po to ne
+        summerhouse = summerhouseEjb.findById(1);
+        System.out.println("OOOOOOO"+summerhouse.getTitle());
+        System.out.println("OOOOOOO"+summerhouse.getCost());
+        
     }
     
     public void findMembersOnSamePeriod() {
@@ -160,24 +183,25 @@ public class ReservationBean implements Serializable {
     
     public String saveReservation(){
         try {
-            System.out.println("aaaa"+summerhouseDetails.getDetailedSummerhouse().getTitle());
             payForReservation(); // 
             Reservation reservation = new Reservation();
             reservation.setAccountId(account);
-            reservation.setSummerhouseId(summerhouseDetails.getDetailedSummerhouse()); //cia cj nepaskolins
+            reservation.setSummerhouseId(this.summerhouse);
             reservation.setStartDate(this.startDate);
             reservation.setEndDate(this.endDate);
-            reservation.setCost(summerhouseDetails.getDetailedSummerhouse().getCost()); // jei bus daugiau savaiciu, nebus taip paprasta
+            reservation.setCost(this.summerhouse.getCost()); // jei bus daugiau savaiciu, nebus taip paprasta
             
-            //tikrinimas ar jau toks yra
             reservationEjb.insertReservation(reservation);
+            reservationEjb.insertReservation2(reservation);
+            
+            em.joinTransaction();
             Message.addSuccessMessage("Rezervacija sėkmingai atlikta!");
         }
         catch (Exception pe) {
             System.out.println("********************************" + pe.getMessage() + pe.getStackTrace().toString());
             Message.addErrorMessage("Nesijaudinkite, bet įvyko nežinoma klaida. Bandykite dar kartą.");
         }
-        return "reservation?faces-redirect=true";
+        return "";
     }
     
     private void payForReservation(){
