35cfc6ccabd53757049034035e56d9ab7a6d3225
alxemist <anthus.ab@gmail.com>
2016-05-28 20:16:15 +0300

    Merge origin/master Payments and reservation cancel Checked stripes
    Conflicts:
    	LabanoroDraugai/src/main/java/DataAccess/EJB/ReservationCRUD.java
    	LabanoroDraugai/src/main/webapp/mySummerhouses.xhtml

--- LabanoroDraugai/src/main/java/BusinessLogic/EJB/MySummerhouses.java
+++ LabanoroDraugai/src/main/java/BusinessLogic/EJB/MySummerhouses.java
@@ -1,58 +1,101 @@
-
 package BusinessLogic.EJB;
 
 import DataAccess.EJB.AccountCRUD;
 import DataAccess.EJB.ReservationCRUD;
+import DataAccess.EJB.SettingsCRUD;
 import DataAccess.EJB.SummerhouseCRUD;
 import DataAccess.JPA.Account;
 import DataAccess.JPA.Reservation;
+import DataAccess.JPA.Settings;
 import DataAccess.JPA.Summerhouse;
+import Interceptors.Interceptable;
 import Messages.Message;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.SynchronizationType;
 import org.primefaces.component.api.UIColumn;
 import org.primefaces.component.datatable.DataTable;
 
 /**
  *
- * @author Liudas 
+ * @author Liudas
  */
-
 @RequestScoped
 @Named
+@Stateful
 public class MySummerhouses implements Serializable {
     
-    @Inject SummerhouseCRUD summerhouseCRUD;
-    @Inject ReservationCRUD reservationCRUD;
-    @Inject AccountCRUD accountCRUD;
-    @Inject CancelReservationBean cancelReservationBean;
+    @PersistenceContext(type=PersistenceContextType.TRANSACTION, synchronization=SynchronizationType.UNSYNCHRONIZED) 
+    private EntityManager em;
     
     @Inject
+    SummerhouseCRUD summerhouseCRUD;
+    @Inject
+    ReservationCRUD reservationCRUD;
+    @Inject
+    AccountCRUD accountCRUD;
+    @Inject
+    CancelReservationBean cancelReservationBean;
+    @Inject
+    SettingsCRUD settingsCRUD;
+
+    @Inject
     LoginBean loginBean;
-    
+
     private String accountFbId;
     private Account acc;
     private List<Reservation> myReservations = new ArrayList();
-    
+
     @PostConstruct
     public void init() {
         accountFbId = loginBean.getFbid();
-        if (accountFbId != null) acc = accountCRUD.findAccount(accountFbId);      
-        myReservations = reservationCRUD.getByAccount(acc); 
+        if (accountFbId != null) {
+            acc = accountCRUD.findAccount(accountFbId);
+        }
+        myReservations = reservationCRUD.getByAccount(acc);
     }
-    public void cancelReservation(Reservation reservation){
-        try{
-        cancelReservationBean.cancelReservation(reservation);
-        myReservations.remove(reservation);
-        }catch(Exception ex){
+
+    @Interceptable
+    public void cancelReservation(Reservation reservation) {
+        try {
+            
+
+            Settings settings = settingsCRUD.findSettings();
+
+            int days = settings.getCancellationTime();
+            Date reservtionEnd = settings.getCloseReservation();
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DAY_OF_MONTH, days);
+
+            Date today = calendar.getTime();
+
+            if (today.after(reservtionEnd)) {
+                Message.addErrorMessage("Rezervacijos atšaukti nebegalima.");
+                return;
+            }
+            cancelReservationBean.cancelReservation(reservation);
+            myReservations.remove(reservation);
+            em.joinTransaction();
+            em.flush();
+            Account account = accountCRUD.findAccount(loginBean.getFbid());
+            FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("interceptorAccount", account);
+        } catch (Exception ex) {
             Message.addErrorMessage("Nesijaudinkite, bet įvyko klaida ir rezervacijos pašalinimas nebuvo atliktas. Bandykite dar kartą.");
             System.out.println("--------------------cancelReservationFail-----------------------");
         }
@@ -81,5 +124,5 @@ public class MySummerhouses implements Serializable {
     public void setMyReservations(List<Reservation> myReservations) {
         this.myReservations = myReservations;
     }
-    
+
 }
