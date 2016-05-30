231337ac27f02c0703b520622cb0dc22c27cde5e
Povilas <Povilas@91.233.179.238>
2016-05-30 18:50:08 +0300

    fix filter

--- LabanoroDraugai/src/main/java/Filters/LoginFilter.java
+++ LabanoroDraugai/src/main/java/Filters/LoginFilter.java
@@ -71,6 +71,7 @@ public class LoginFilter implements Filter {
         String addServiceURI = request.getContextPath() + "/addService.xhtml";
         String editServiceURI = request.getContextPath() + "/editService.xhtml";
         String deleteServiceURI = request.getContextPath() + "/deleteService.xhtml";
+        String advBoardURI = request.getContextPath() + "/advBoard.xhtml";
         int indexOfPay = request.getRequestURI().indexOf("payMembershipFee");
         int indexOfRes = request.getRequestURI().indexOf("resources");
 
@@ -112,6 +113,7 @@ public class LoginFilter implements Filter {
         boolean addServiceRequest = request.getRequestURI().equals(addServiceURI);
         boolean editServiceRequest = request.getRequestURI().equals(editServiceURI);
         boolean deleteServiceRequest = request.getRequestURI().equals(deleteServiceURI);
+        boolean advBoardRequest = request.getRequestURI().equals(advBoardURI);
         boolean admin = false;
         boolean candidate = false;
         boolean member = false;
@@ -153,7 +155,7 @@ public class LoginFilter implements Filter {
                 chain.doFilter(request, response);
             } else if ((confirmPaymentsRequest || removeMessageRequest || addPaymentsRequest || addServiceRequest || editServiceRequest || deleteServiceRequest) && admin) {
                 chain.doFilter(request, response);
-            } else if ((myServicesRequest || servicesRequest || addMessageRequest) && (admin || member)) {
+            } else if ((myServicesRequest || servicesRequest || addMessageRequest || advBoardRequest) && (admin || member)) {
                 chain.doFilter(request, response);
             } else {
                 response.sendRedirect(pageNotFoundURL);
